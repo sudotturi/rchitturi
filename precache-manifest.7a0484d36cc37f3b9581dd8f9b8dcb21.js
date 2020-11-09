b78f15cfde6177c8f7c41faa13536e7aea6d1b64
Rahul2309 <rahulchitturi420@gmail.com>
2020-11-09 04:10:10 +0000

    Updates

---
+++ precache-manifest.7a0484d36cc37f3b9581dd8f9b8dcb21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4ead7b0f935d7283e1df8c1134602f",
+    "url": "/rchitturi/index.html"
+  },
+  {
+    "revision": "8fb9ad9ab809c7464357",
+    "url": "/rchitturi/static/css/2.da2b8a69.chunk.css"
+  },
+  {
+    "revision": "837c1fd4eb1205c909cf",
+    "url": "/rchitturi/static/css/main.1fb65ca3.chunk.css"
+  },
+  {
+    "revision": "8fb9ad9ab809c7464357",
+    "url": "/rchitturi/static/js/2.85a9e294.chunk.js"
+  },
+  {
+    "revision": "978de3a8cab90dd655d2ecf53d430bc5",
+    "url": "/rchitturi/static/js/2.85a9e294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837c1fd4eb1205c909cf",
+    "url": "/rchitturi/static/js/main.1979325e.chunk.js"
+  },
+  {
+    "revision": "ce0eba3e540c7d5ee13a",
+    "url": "/rchitturi/static/js/runtime-main.2a842c64.js"
+  }
+]);
