313ebbd6be7ed4c818a6988478d7a177c0738fe2
RAHUL C H <46349786+Rahul2309@users.noreply.github.com>
2020-11-21 00:18:07 +0530

    Commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,18 +3,54 @@ import "./header.css";
 import { properties } from "../../utils/properties";
 import { Card } from "react-bootstrap";
 import history from "../../utils/history";
-import { Header } from "rsuite";
+import { Header, Drawer, Button, Icon } from "rsuite";
+
 class Headerbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backdrop: false,
+      show: false
+    };
+    this.close = this.close.bind(this);
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+  }
+  close() {
+    this.setState({
+      show: false
+    });
+  }
+  toggleDrawer() {
+    this.setState({ show: true });
+  }
+
   render() {
+    const { show } = this.state;
     return (
       <Header>
         <Card className="header">
+          <button className="togglebutton" onClick={this.toggleDrawer}>
+            <Icon icon="bars" size="1x" />
+          </button>
           <div id="logo" onClick={() => history.push("/")}>
             <b id="C">{properties.projectsplitone}</b>
             <b id="HITTU">{properties.projectsplittwo}</b>
             <b id="RI">{properties.projectsplitthree}</b>
           </div>
         </Card>
+        <Drawer
+          backdrop="true"
+          placement="left"
+          show={show}
+          size="xs"
+          onHide={this.close}
+          className="tog"
+        >
+          <Drawer.Header>
+            <Drawer.Title>Drawer Title</Drawer.Title>
+          </Drawer.Header>
+          <Drawer.Body></Drawer.Body>
+        </Drawer>
       </Header>
     );
   }
